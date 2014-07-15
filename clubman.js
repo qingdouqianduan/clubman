f924791551bbf130aa5f38b0f2ab473a767df880
damon <4039293@qq.com>
2014-07-15 23:42:54 +0800

    all domain

---
+++ clubman.js
@@ -0,0 +1,27 @@
+void function(global,Doc){
+    var o_ = global._
+        ,NsKey = Doc.URL.replace(/(#.+|\W)/g, "")
+        ,NsVal = global[NsKey];
+        
+        club = global.club = global._ = function(){
+          return new _();     
+        }
+    
+        _.aa = function(){
+          return 11;
+        }
+  
+
+
+
+     
+
+ 
+   
+      
+  
+
+
+
+
+}(self,self.document)
