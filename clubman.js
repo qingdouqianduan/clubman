64e6035694e7cf075e39d442e0c37eaa3ad4bd4f
Damon yao <4039293@qq.com>
2014-07-16 15:09:57 +0800

    add mixIn function

--- clubman.js
+++ clubman.js
@@ -1,16 +1,56 @@
 void function(global,Doc){
     var o_ = global._
-        ,NsKey = Doc.URL.replace(/(#.+|\W)/g, "")
-        ,NsVal = global[NsKey];
-        
-        club = global.club = global._ = function(){
-          return new _();     
-        }
-    
-        _.aa = function(){
-          return 11;
-        }
-  
+        ,oClub = global.club
+        ,op = Object.prototype
+        ,hasOwn = op.hasOwnProperty;
+ 	/**
+	 * 主构造函数
+   */		       
+    var _ = function(){
+       return new _();     
+    };
+  /**
+    * 判断对象是否拥有某属性
+    * @param {Object} obj
+    * @param {propertyName} proto
+    * @return {boolean} 
+    */    
+    _.hasOwn = function(obj,proto){
+       return hasOwn.call(obj,proto);
+    }
+	/**
+	 * 耦合对象
+	 * @param {Object} target 目标
+   * @param {Object} source 源
+   * @return {Object} target  				
+	 */		   
+    _.mixIn = function(target,source){
+      var argu = [].slice.call(arguments)
+          ,i=1 //跳过target
+          ,key //相同key 不覆盖
+          ,cover = typeof argu[argu.length - 1] === 'boolean' ? argu.pop() : true;//是否为布尔值,是则反悔,不是则为默认
+          while ((source = argu[i++])){
+            for (key in source){ 
+                if (_.hasOwn(source, key) && (cover || !(key in target))) {
+                    target[key] = source[key];
+                }
+             }
+          }
+        return target;                                
+    }	 
+  /**
+   * 核心模块添加
+   */   
+   _.mixIn(_,{
+      //当前版本号    
+      version : '1.0'
+      ,
+
+
+   }) 
+ 
+
+
 
 
 
@@ -20,7 +60,14 @@ void function(global,Doc){
    
       
   
-
+      /**
+       * 添加到公共对象
+       */
+ 
+      global._ = global.club = _;						
+	
+	
+	
 
 
 
